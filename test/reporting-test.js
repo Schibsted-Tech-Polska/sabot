abf01b4de0a106552a6918a329f8af37521f46c1
Jakub Wasilewski <wasyl@wasyl.eu>
2015-06-12 13:43:27 +0200

    Reporting conversions reacts to promise success/rejection properly now.

--- test/reporting-test.js
+++ test/reporting-test.js
@@ -12,6 +12,58 @@ describe("Reporting ", function() {
     }
 
     sabot.reportThroughCallbacks(assignments, mockStorage(), loadCallback, function(){});
+
     assert.deepEqual(gatheredInfo, assignments);
   });
+
+  it("should report outstanding conversions", function() {
+    var storage = mockStorage({
+      'sabotOutstandingConversions': [
+        {test: '1', variant: 'b'}
+      ]
+    });
+
+    var conversionsReported = [];
+    function conversionCallback(test, variant) {
+      conversionsReported.push(test + variant);
+    }
+
+    sabot.reportThroughCallbacks({}, storage, function(){}, conversionCallback);
+
+    assert.deepEqual(conversionsReported, ['1b']);
+  });
+
+  it("should remove successfully reported conversions from storage", function() {
+    var storage = mockStorage({
+      'sabotOutstandingConversions': [
+        {test: '1', variant: 'b'}
+      ]
+    });
+
+    function conversionCallback() {
+      return $.Deferred().resolve("Ok!");
+    }
+
+    sabot.reportThroughCallbacks({}, storage, function(){}, conversionCallback);
+
+    assert.deepEqual(storage.getItem('sabotOutstandingConversions'), []);
+  });
+
+  it("should not remove conversions if reporting fails", function() {
+    var storage = mockStorage({
+      'sabotOutstandingConversions': [
+        {test: '1', variant: 'b'}
+      ]
+    });
+
+    function conversionCallback() {
+      return $.Deferred().fail("Argh!");
+    }
+
+    sabot.reportThroughCallbacks({}, storage, function(){}, conversionCallback);
+
+    assert.deepEqual(storage.getItem('sabotOutstandingConversions').length, 1);
+  });
+
+
 });
