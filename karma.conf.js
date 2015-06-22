5e3e8475467eefca912870d0c2656c8a29d8bfaa
Jakub Wasilewski <wasyl@wasyl.eu>
2015-06-22 13:32:14 +0200

    Test coverage using karma-coverage now set up.

--- karma.conf.js
+++ karma.conf.js
@@ -32,14 +32,19 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      '**/*.html': ['html2js']
+      '**/*.html': ['html2js'],
+      'dist/sabot.js': ['coverage']
     },
 
+    coverageReporter: {
+      type : 'html',
+      dir : 'coverage/'
+    },
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['mocha'],
+    reporters: ['mocha', 'coverage'],
 
 
     // web server port
